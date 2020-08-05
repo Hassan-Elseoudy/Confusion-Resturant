9713bce9448ae9a695f609684e6c8df72eb31300
Hassan Elseoudy <hassan.elseoudy@sia-world.com>
2020-08-05 16:20:13 +0200

    Promise Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,8 +21,8 @@ export class HomeComponent implements OnInit {
   leader: Leader;
 
   ngOnInit(): void {
-    this.dishService.getDishFatured().then((dish) => this.dish = dish);
-    this.promotionService.getPromotionFatured().then((promotion) => this.promotion = promotion);
+    this.dishService.getFeaturedDish().then((dish) => this.dish = dish);
+    this.promotionService.getFeaturedPromotion().then((promotion) => this.promotion = promotion);
     this.leaderService.getFeaturedLeader().then((leader) => this.leader = leader);
   }
 
