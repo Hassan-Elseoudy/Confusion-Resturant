fd09c998664161c24b4cc68438d0e32192fa30de
Hassan Elseoudy <hassan.elseoudy@sia-world.com>
2020-08-06 13:01:27 +0200

    Fixing Validators

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -48,13 +48,14 @@ export class ContactComponent implements OnInit {
     const form = this.feedbackForm;
     for (const field in this.formErrors) {
       if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
         this.formErrors[field] = '';
         const control = form.get(field);
-        if (control && control.dirty && control.invalid) {
-          const message = this.validationMessages[field];
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
           for (const key in control.errors) {
             if (control.errors.hasOwnProperty(key)) {
-              this.formErrors[field] += message[key] + ' ';
+              this.formErrors[field] += messages[key] + ' ';
             }
           }
         }
@@ -71,17 +72,16 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstName: ['', Validators.required, Validators.minLength(2), Validators.maxLength(25)],
-      lastName: ['', Validators.required, Validators.minLength(2), Validators.maxLength(25)],
-      telNum: [0, Validators.required, Validators.pattern],
-      email: ['', Validators.required, Validators.email],
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telNum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: [false, Validators.required],
       contactType: ['None', Validators.required],
       message: ['', Validators.required]
     });
     this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
-
-    this.onValueChanged()
+    this.onValueChanged();
   }
 
   onSubmit() {
